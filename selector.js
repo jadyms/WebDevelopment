ab952696281ba2aa1ab95daa86fcd752a2c48ed0
jadyms <33762825+jadyms@users.noreply.github.com>
2019-09-26 10:17:28 +0100

    Selectors
    Selecting elements using jQuery

---
+++ selector.js
@@ -0,0 +1,11 @@
+// Select all div and change background to purple
+$("div").css("background", "purple");
+
+//Select everything with class=highlight
+$(".highlight").css("width", "200px");
+
+// Select id=third
+$("#third").css("border", "solid orange");
+
+// Select only the first element of div tag
+$("div:first-of-type").css("color", "pink");
