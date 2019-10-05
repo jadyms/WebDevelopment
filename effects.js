d19c9fb52f47faf0b55f4565f96977312fa75513
jadyms <33762825+jadyms@users.noreply.github.com>
2019-10-05 14:54:08 +0100

    jQuery Effects - Fade effects
    fadeOut()
    fadeIn()
    fadeToggle()
    remove()

---
+++ effects.js
@@ -0,0 +1,16 @@
+//For each button clicked
+$("button").on("click", function(){
+	// fade out the div tags in 1 sec
+	// after the 1 sec, then print the message
+	// *this just fade the element out
+	$("div").fadeOut(1000, function(){
+		console.log("Fade completed");
+		// remove the element
+		$(this).remove();
+
+		// fadeIn() works the opposite way
+		// fadeToggle() toggles (in if it is out and vice versa)
+
+	});
+	
+});
