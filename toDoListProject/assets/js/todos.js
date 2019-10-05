a7e22c0aca86f004d0fd726bf31bb0e7a52687de
jadyms <33762825+jadyms@users.noreply.github.com>
2019-10-05 16:09:14 +0100

    To Do Project - toggleClass

--- toDoListProject/assets/js/todos.js
+++ toDoListProject/assets/js/todos.js
@@ -1,19 +1,7 @@
 // cross over the done todo
 $("li").click(function(){
-	// if color is gray
-	if($(this).css("color") === "rgb(128, 128, 128)"){
-		// turn it black
-		$(this).css({
-		color: "black",
-		textDecoration: "none"
-	});
 
-		// if not gray
-	}else{$(this).css({
-		// turn it gray
-		color: "gray",
-		textDecoration: "line-through"
-	});
-}
-	
+	$(this).toggleClass("completed")
+
+		
 });
