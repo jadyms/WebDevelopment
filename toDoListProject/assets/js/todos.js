176252ebbfbf85280ccf3715a9529652f21990d8
jadyms <33762825+jadyms@users.noreply.github.com>
2019-10-05 16:02:23 +0100

    To Do Project - Line through

--- toDoListProject/assets/js/todos.js
+++ toDoListProject/assets/js/todos.js
@@ -1,1 +1,19 @@
-alert("yey");
+// cross over the done todo
+$("li").click(function(){
+	// if color is gray
+	if($(this).css("color") === "rgb(128, 128, 128)"){
+		// turn it black
+		$(this).css({
+		color: "black",
+		textDecoration: "none"
+	});
+
+		// if not gray
+	}else{$(this).css({
+		// turn it gray
+		color: "gray",
+		textDecoration: "line-through"
+	});
+}
+	
+});
