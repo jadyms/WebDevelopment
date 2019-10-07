3a0302f68df581e11c485de95b6ba2b2711fc73d
jadyms <33762825+jadyms@users.noreply.github.com>
2019-10-07 21:56:24 +0100

    To Do Project - Stop Propagation
    parent() - element parent
    e.stopPropagation() - if not applied, events would be applied to the parent, then body

--- toDoListProject/assets/js/todos.js
+++ toDoListProject/assets/js/todos.js
@@ -1,7 +1,16 @@
 // cross over the done todo
 $("li").click(function(){
-
+	//add/remove completed class to clicked element
 	$(this).toggleClass("completed")
-
 		
+});
+
+// Click on x to delete todo
+$("span").click(function(e){
+	//remove the li that contains the span
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	//avoid bubbling up 
+	e.stopPropagation();
 });
